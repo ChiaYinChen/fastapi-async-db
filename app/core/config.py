81aa4a2bf32559b0e0a4688632d1a30d708513f2
carol <awdrg1210@gmail.com>
2022-01-13 11:43:51 +0800

    set database and config for test cases

--- app/core/config.py
+++ app/core/config.py
@@ -8,6 +8,16 @@ class Settings(BaseSettings):
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
-    SQLALCHEMY_DATABASE_URI: str = "sqlite:///./test.db"
+    SQLALCHEMY_DATABASE_URI: str = "sqlite:///./prod.db"
+
+    TESTING: bool = False
+    TEST_SQLALCHEMY_DATABASE_URI: str = "sqlite:///./test.db"
+    TEST_USER_EMAIL: str = "test@example.com"
+    TEST_USER_PASSWORD: str = "testuserpass"
+
+    class Config:
+        env_file = ".env"
+        case_sensitive = True
+
 
 settings = Settings()
--- app/db/session.py
+++ app/db/session.py
@@ -4,7 +4,10 @@ from sqlalchemy import create_engine, MetaData
 
 from ..core.config import settings
 
-database = Database(settings.SQLALCHEMY_DATABASE_URI)
+if settings.TESTING:
+    database = Database(settings.TEST_SQLALCHEMY_DATABASE_URI, force_rollback=True)  # noqa: E501
+else:
+    database = Database(settings.SQLALCHEMY_DATABASE_URI)
 metadata = MetaData()
 
 engine = create_engine(
