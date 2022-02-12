71c6110501ef9899c3430f2f3f00d73017ffcabe
carol <awdrg1210@gmail.com>
2022-02-12 20:47:37 +0800

    router for logout

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 """Main app."""
 from fastapi import FastAPI
 
-from .api import hello, login, post, user
+from .api import hello, login, logout, post, user
 from .db.session import database, engine, metadata
 
 metadata.create_all(bind=engine)
@@ -25,3 +25,4 @@ app.include_router(hello.router)
 app.include_router(user.router, prefix="/users", tags=["users"])
 app.include_router(post.router, prefix="/posts", tags=["posts"])
 app.include_router(login.router, tags=["login"])
+app.include_router(logout.router, tags=["logout"])
